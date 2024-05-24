593403007cc8163e1fd06f6c13db556e9647f729
davidgomezcode <davidgomezcode@gmail.com>
2024-05-24 10:15:29 -0500

    Se cambian estilos de botones y agregan imágenes -torre y mano-. Se cambia el color de los dados.

--- script.js
+++ script.js
@@ -105,7 +105,7 @@ btnHold.addEventListener('click', function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
 
-    if (scores[activePlayer] >= 100) {
+    if (scores[activePlayer] >= 20) {
       //If a player gets to 100 wins
       //its backgorund turns black:
       document
